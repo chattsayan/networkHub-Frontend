f413e2fe9d1e679710a0549235d895f9df9735aa
sayan <chatterjeesayan730@gmail.com>
2025-01-24 20:04:43 +0530

    base_url update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,8 @@
-export const BASE_URL = "/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+// for production
+// export const BASE_URL = "/api";
+
+// for dev
+// export const BASE_URL = "http://localhost:7777";
